7c02afb69fd5d6c8bac87994cc7f38636ff829ba
kxtarrao <kyletarrao@cyclic-automation.io>
2025-01-08 02:04:54 -0500

    Set up website app to run gunicorn

--- rxconfig.py
+++ rxconfig.py
@@ -5,7 +5,7 @@ config = rx.Config(
     app_name="website",
     backend_host="0.0.0.0",  # Ensure it's accessible externally
     backend_port=int(os.environ.get("PORT", 8000)),  # Use Render's dynamic PORT
-    frontend_port=3000,
+    frontend_port=1000,
     debug=False,
     dev_mode=False,
 )
