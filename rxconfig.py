dcb101f35c1c0502d03a8529aa84877b4e9a9367
kxtarrao <kyletarrao@cyclic-automation.io>
2025-01-09 17:25:49 -0500

    changes

--- rxconfig.py
+++ rxconfig.py
@@ -4,8 +4,8 @@ import reflex as rx
 config = rx.Config(
     app_name="website",
     backend_host="0.0.0.0",  # Ensure it's accessible externally
-    # backend_port=int(os.environ.get("PORT", 8000)),  # Use Render's dynamic PORT
-    # frontend_port=10000,
+    frontend_port=10000,
+    backend_port=10000,
     debug=False,
     dev_mode=False,
 )
