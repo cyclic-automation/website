3b7a1c951917da30456565d62e992619ce45c3f3
kxtarrao <kyletarrao@cyclic-automation.io>
2025-01-11 14:07:19 -0500

    changes

--- rxconfig.py
+++ rxconfig.py
@@ -3,9 +3,9 @@ import reflex as rx
 config = rx.Config(
     app_name="website",
     frontend_host="0.0.0.0",
-    # backend_host="0.0.0.0",
+    backend_host="0.0.0.0",
     frontend_port=10000,
-    # backend_port=8000,
+    backend_port=8000,
     debug=False,
     dev_mode=False,
 )
