03d2cc192b5f26d7d06330a31a5f0a5e628f5ac6
kxtarrao <kyletarrao@cyclic-automation.io>
2025-01-11 16:07:15 -0500

    changes

--- rxconfig.py
+++ rxconfig.py
@@ -4,8 +4,8 @@ config = rx.Config(
     app_name="website",
     frontend_host="0.0.0.0",
     # backend_host="0.0.0.0",
-    frontend_port=3000,
-    backend_port=8000,
+    frontend_port=8000,
+    backend_port=3000,
     debug=False,
     dev_mode=False,
 )
