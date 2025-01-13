6dc6ce6edb2a040130f2b3f1b82d87d093e39307
kxtarrao <kyletarrao@cyclic-automation.io>
2025-01-13 13:17:15 -0500

    changes

--- rxconfig.py
+++ rxconfig.py
@@ -3,7 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="website",
     deploy_url="https://website-epp6.onrender.com",
-    # backend_host="0.0.0.0",
+    backend_host="0.0.0.0",
     frontend_port=8000,
     backend_port=3000,
     debug=False,
