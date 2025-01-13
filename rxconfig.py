6c72f5d147cbe1083ed53af20c5f660311f282d9
kxtarrao <kyletarrao@cyclic-automation.io>
2025-01-13 01:52:55 -0500

    changes

--- rxconfig.py
+++ rxconfig.py
@@ -4,8 +4,8 @@ config = rx.Config(
     app_name="website",
     deploy_url="https://website-epp6.onrender.com",
     # backend_host="0.0.0.0",
-    frontend_port=8000,
-    backend_port=3000,
+    # frontend_port=8000,
+    # backend_port=3000,
     debug=False,
     dev_mode=False,
 )
