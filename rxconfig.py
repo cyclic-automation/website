0543cde618cbaa95ba66cfad7b555780e47dff0c
kxtarrao <kyletarrao@cyclic-automation.io>
2025-01-16 15:59:50 -0500

    changes

--- rxconfig.py
+++ rxconfig.py
@@ -4,8 +4,8 @@ import reflex as rx
 config = rx.Config(
     app_name="website",
     deploy_url="https://website-mflc.onrender.com",
-    # frontend_port=3000,
-    # backend_port=8000,
+    frontend_port=3000,
+    backend_port=8000,
     debug=False,
     dev_mode=False,
 )
